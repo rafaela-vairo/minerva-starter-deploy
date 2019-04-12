5380c5016f0d029e58af526cc2c35062489e7df8
Luiz dos Santos <luizsantos@localhost.localdomain>
2019-04-12 10:21:46 -0300

    formulário netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,6 +38,11 @@ export default ({ data }) => {
           </div>
         ))}
       </div>
+      <form name="contact" netlify> 
+      <p><label>Name <input type="text" name="name" /></label></p> 
+      <p><label>Email <input type="email" name="email" /></label></p>
+      <p><button type="submit">Send</button></p> 
+      </form>
     </Layout>
   )
 }
