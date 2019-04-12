001d19b6d5769ffcc940aa1e3721168f99ef7cf0
Luiz dos Santos <luizsantos@localhost.localdomain>
2019-04-12 10:33:39 -0300

    form debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,24 +38,18 @@ export default ({ data }) => {
           </div>
         ))}
       </div>
-      <form name="contact" netlify> 
+      <form name="contact" data-netlify="true"> 
         <p><label>Name <input type="text" name="name" /></label></p> 
         <p><label>Email <input type="email" name="email" /></label></p>
         <p><button type="submit">Send</button></p> 
       </form>
 
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="POST" data-netlify="true" action="/">
         <p>
           <label>Your Name: <input type="text" name="name" /></label>   
         </p>
         <p>
           <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
         </p>
         <p>
           <label>Message: <textarea name="message"></textarea></label>
